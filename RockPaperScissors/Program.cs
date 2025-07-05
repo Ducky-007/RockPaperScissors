e17a1fa30290b96b9a8562d881b21d10a99f9657
Ducky-007 <c.duckett93@gmail.com>
2025-07-05 10:34:58 -0700

    Initial commit

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,25 @@
+﻿// creates app for Rock Paper Scissors game
+using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Game
+    {
+        public void Start()
+        {
+            Console.WriteLine("Welcome to Rock Paper Scissors!");
+            // Game logic goes here
+        }
+    }
+}
